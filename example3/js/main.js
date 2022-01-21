9ca0066d61a976ea131218a7c265b610145013f8
asakew <asauz777@gmail.com>
2022-01-21 10:49:35 +0500

    fetch: response.name, Fixeeeeed 😎 Mirjalol akaga raxmat)

--- example3/js/main.js
+++ example3/js/main.js
@@ -1,29 +1,18 @@
 function getElement(id) {
   return document.getElementById(id);
 }
-
 fetch("https://weatherapi-com.p.rapidapi.com/history.json?q=London&dt=2022-01-19&lang=en", {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
-        "x-rapidapi-key": "9683bbb1a2mshfb8d32ce50562fdp110573jsn1c75e3c78012"
-      }
-    })
-    .then(response => response.json())
-    .then(response => {
-      console.log(response);
-
-
-getElement('name').innerHTML = 'Name: ' + response.name;
-getElement('country').innerHTML = 'Country: ' + response.country;
-getElement('region').innerHTML = 'Region: ' + response.region;
-getElement('localtime').innerHTML = 'localtime: ' + response.localtime;
-
-    })
-    .catch(err => {
-      console.error(err);
-    });
-    
-
-
-    
+  "method": "GET",
+  "headers": {
+    "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
+    "x-rapidapi-key": "9683bbb1a2mshfb8d32ce50562fdp110573jsn1c75e3c78012"
+  }
+}).then(response => response.json()).then(response => {
+  console.log(response);
+  getElement('name').innerHTML = 'Name: ' + response.location.name;
+  getElement('country').innerHTML = 'Country: ' + response.location.country;
+  getElement('region').innerHTML = 'Region: ' + response.location.region;
+  getElement('localtime').innerHTML = 'localtime: ' + response.location.localtime;
+}).catch(err => {
+  console.error(err);
+});
